1a453df2b8f50218d24928cd64e8f796f629846f
Brendan Caffrey <brendan@litmus.com>
2022-12-11 19:00:08 -0500

    day 12 prep

---
+++ AdventOfCode2022/Day12/Task1.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode2022.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day12
+{
+    public class Task1 : AdventOfCodeTaskBase
+    {
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
